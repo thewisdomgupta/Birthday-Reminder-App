1e4a9287fb71f790ef64ca19c27a5251422fca3d
thewisdomgupta <vivekkumarguptafromktr@gmail.com>
2022-07-15 02:09:19 +0530

    New Image Added

---
+++ src/data.js
@@ -0,0 +1,44 @@
+import Vivek from './Images/Vivek.jpeg'
+import Nikita from './Images/Nikita.jpg'
+import Supriya from './Images/Supriya.jpg'
+import Vishal from './Images/Vishal.jpg'
+import Jyotsna from './Images/Jyotsna.jpg'
+export default [
+  {
+    id: 1,
+    name: 'Vivek Gupta',
+    age: '16 September',
+    image: Vivek,
+  },
+  {
+    id: 2,
+    name: 'Nikita',
+    age: '13 July',
+    image: Nikita,
+  },
+  {
+    id: 3,
+    name: 'Supriya',
+    age: '25 July',
+    image: Supriya,
+  },
+  {
+    id: 4,
+    name: 'Vishal',
+    age: '18 September',
+    image: Vishal,
+  },
+  {
+    id: 5,
+    name: 'Trisha',
+    age: '19 January',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+  {
+    id: 6,
+    name: 'Jyotsna',
+    age: '27 April',
+    image: Jyotsna,
+  },
+];
