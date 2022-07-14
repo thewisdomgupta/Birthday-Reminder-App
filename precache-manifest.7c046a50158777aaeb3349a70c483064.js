a4a96b325d56cf4d2e1f64f32c126c6935787500
thewisdomgupta <vivekkumarguptafromktr@gmail.com>
2022-07-15 01:52:21 +0530

    Updates

---
+++ precache-manifest.7c046a50158777aaeb3349a70c483064.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d295af96c9abad0392b9379a0d6d1dc",
+    "url": "/Birthday-Reminder-App/index.html"
+  },
+  {
+    "revision": "2fc8837e9ce3dfe2ca27",
+    "url": "/Birthday-Reminder-App/static/css/main.2ca271ef.chunk.css"
+  },
+  {
+    "revision": "8252c03711ed15b4a81a",
+    "url": "/Birthday-Reminder-App/static/js/2.74e3fc11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Birthday-Reminder-App/static/js/2.74e3fc11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc8837e9ce3dfe2ca27",
+    "url": "/Birthday-Reminder-App/static/js/main.19b7030b.chunk.js"
+  },
+  {
+    "revision": "2209d7f48826cd4e597c",
+    "url": "/Birthday-Reminder-App/static/js/runtime-main.b0e5b619.js"
+  },
+  {
+    "revision": "a64e46dac5c397cfcb813435df2a5a4e",
+    "url": "/Birthday-Reminder-App/static/media/Nikita.a64e46da.jpg"
+  },
+  {
+    "revision": "63eb661a228dd5e06f47af5b97a11215",
+    "url": "/Birthday-Reminder-App/static/media/Supriya.63eb661a.jpg"
+  },
+  {
+    "revision": "025f6e573c1dd08c083e1dad9265b817",
+    "url": "/Birthday-Reminder-App/static/media/Vishal.025f6e57.jpg"
+  },
+  {
+    "revision": "fb5866b7d8f3c208c34922161fad205e",
+    "url": "/Birthday-Reminder-App/static/media/Vivek.fb5866b7.jpeg"
+  }
+]);
